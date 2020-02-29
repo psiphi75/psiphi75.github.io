e807efe63ce83059c3e33989a60aa6bfe54f588a
Simon Werner <simonwerner@gmail.com>
2020-03-01 07:00:39 +1300

    Demo

---
+++ banana-cover/precache-manifest.c669d925e4244e25323ae4d562aee158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a407d4ea0d58887fb94b6ee292c1ab10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53e591a1d1e69d6d9be",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "abda8dc007c45ea1f6a0",
+    "url": "/static/css/main.86ac44d1.chunk.css"
+  },
+  {
+    "revision": "e53e591a1d1e69d6d9be",
+    "url": "/static/js/2.c5e1546e.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.c5e1546e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abda8dc007c45ea1f6a0",
+    "url": "/static/js/main.4a00479e.chunk.js"
+  },
+  {
+    "revision": "11db4e972acc27e9a829",
+    "url": "/static/js/runtime-main.f41f0cf7.js"
+  }
+]);
